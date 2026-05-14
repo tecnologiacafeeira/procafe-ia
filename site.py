1b959a326088aadf377d589f7a30b7760272d1b1
tecnologiacafeeira <tecnologiacafeeira@gmail.com>
2026-05-14 15:54:31 -0300

    Update site.py

--- site.py
+++ site.py
@@ -258,7 +258,7 @@ st.markdown("""
 <div class="hero">
     <div class="hero-content">
         <div class="logo-circle">
-    <img src="https://raw.githubusercontent.com/tecnologiacafeeira/procafe-ia/principal/assets/logo.png" class="logo-img">
+    <img src="https://raw.githubusercontent.com/tecnologiacafeeira/procafe-ia/main/assets/logo.png" class="logo-img">
 </div>
         <div>
             <h1>FUNDAÇÃO PROCAFÉ</h1>
