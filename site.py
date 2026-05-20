024ec0c0c91b1978600a8d8c634e92e71d264440
tecnologiacafeeira <tecnologiacafeeira@gmail.com>
2026-05-20 16:33:03 -0300

    Update site.py

--- site.py
+++ site.py
@@ -115,11 +115,11 @@ for rec in gerar_recomendacoes(item):
     else:
         elementos.append(Paragraph(f"- {rec_pdf}", estilos["Normal"]))
 
-elementos.append(Spacer(1, 22))
+ elementos.append(Spacer(1, 22))
 
-doc.build(elementos)
-buffer.seek(0)
-return buffer
+ doc.build(elementos)
+ buffer.seek(0)
+ return buffer
 
 
 logo_base64 = carregar_logo_base64("assets/logo.png")
